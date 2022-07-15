3ad4e21aa9f382055d8543fe4c1bd5709c81d8b8
Mahmud Syakiran <mahmudsyakiran407@gmail.com>
2022-07-16 07:17:54 +0800

    Update Script

--- Assets/Kiran - EnemyAI Script/EnemyMovement.cs
+++ Assets/Kiran - EnemyAI Script/EnemyMovement.cs
@@ -13,6 +13,8 @@ public class EnemyMovement : MonoBehaviour
 
     [SerializeField] private bool isMovingRight = true;
 
+    [SerializeField] EnemyTypes currentEnemyType;
+
     void Start()
     {
        
@@ -20,8 +22,8 @@ public class EnemyMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-      
-        var enemyTypes = EnemyTypes.RangedAI;
+
+        var enemyTypes = currentEnemyType;
 
         switch (enemyTypes)
         {
@@ -36,9 +38,9 @@ public class EnemyMovement : MonoBehaviour
                 break;
         }
 
-        Debug.Log("=============");
+       /* Debug.Log("=============");
         Debug.Log("Enemy Types: " + enemyTypes);
-        Debug.Log("=============");
+        Debug.Log("=============");*/
 
     }
 
