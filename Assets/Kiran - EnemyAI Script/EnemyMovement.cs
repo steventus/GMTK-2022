711dc61d6d85e92267039276e5b2a22d011c7575
Steventus <runescape_dudez@hotmail.com>
2022-07-16 18:38:32 +0800

    Revert "Merge branch 'main' into Steventus-branch"
    This reverts commit 78c6ecd2e16c1c71ee681f439ae752f26b4b467c, reversing
    changes made to 24033eadc77553066856799f2230ca4ee4bf864d.

--- Assets/Kiran - EnemyAI Script/EnemyMovement.cs
+++ Assets/Kiran - EnemyAI Script/EnemyMovement.cs
@@ -7,34 +7,24 @@ public class EnemyMovement : MonoBehaviour
 {
     enum EnemyTypes { MeleeAI, RangedAI };
 
-    enum UpgradeEnemy { Base, UpgradeOne, UpgradeTwo, UpgradeThree, UpgradeFour };
+    enum MovementTypes { ChasePlayer, RunFromPlayer };
 
     enum GunTypes { Shotgun, AutoFire, BurstFire, Sniper };
 
     [SerializeField] private bool isMovingRight = true;
-
-    [SerializeField] Transform playerTransform;
     
     [SerializeField] EnemyTypes currentEnemyType;
 
-    [SerializeField] UpgradeEnemy currentUpgradeEnemy;
-
-    [SerializeField] bool canCharge;
-
-    [SerializeField] bool IsAvailable = true;
-
-    [SerializeField] float CooldownDuration = 1.0f;
-
     void Start()
     {
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
     void FixedUpdate()
     {
 
        var enemyTypes = currentEnemyType;
-      
+
         switch (enemyTypes)
         {
             case EnemyTypes.MeleeAI:
@@ -52,120 +42,27 @@ public class EnemyMovement : MonoBehaviour
    
     public void ChasePlayerEnemyMovement()
     {
-        float distance = Vector2.Distance(this.gameObject.transform.position, playerTransform.position);
-        float speed = 3f * Time.deltaTime;
-        Vector3 offset = transform.position - playerTransform.position;
-        var upgradeTypes = currentUpgradeEnemy;
+        float distance = Vector2.Distance(this.gameObject.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
 
         if (distance < 6f)
         {
-            switch (upgradeTypes)
-            {
-                case UpgradeEnemy.Base:
-                    transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed);
-                    break;
-                case UpgradeEnemy.UpgradeOne:
-                    transform.position = Vector2.Lerp(transform.position, playerTransform.position, speed);
-                    break;
-                case UpgradeEnemy.UpgradeTwo:
-                    
-                    transform.position = Vector2.Lerp(transform.position, playerTransform.position, speed);
-                    break;
-                case UpgradeEnemy.UpgradeThree:
-
-                    transform.position = Vector3.Lerp(transform.position + offset, playerTransform.position, speed).normalized;
-                    break;
-                case UpgradeEnemy.UpgradeFour:
-
-                    break;
-                default:
-                    Debug.Log("Upgrade Types not valid");
-                    break;
-            }
+            float speed = 3f * Time.deltaTime;
+            transform.position = Vector2.Lerp(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, speed);
         }
-
-        if(currentUpgradeEnemy == UpgradeEnemy.UpgradeTwo)
-        {
-            StartCoroutine(ChargeAttack());
-            canCharge = true;
-        } 
-        else
-        {
-            canCharge = false;
-        }
-
-        /*Debug.Log(upgradeTypes);*/
-        /*Debug.Log(canCharge);*/
     }
 
     public void RunFromPlayerEnemyMovement()
     {
         float distance = Vector2.Distance(this.gameObject.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position);
-        var upgradeTypes = currentUpgradeEnemy;
-        float speed = 10f * Time.deltaTime;
-
-
+        
         if (distance < 3f)
         {
-            switch (upgradeTypes)
-            {
-                case UpgradeEnemy.Base:
-                  
-                    var enemyPosition = transform.position;
-                    var playerPosition = playerTransform.position;
-                    var dist = (enemyPosition - playerPosition).magnitude;
-                    var mapped = Mathf.InverseLerp(10, 5, dist);
-                    transform.position = Vector2.MoveTowards(enemyPosition, playerPosition, -mapped * speed);
-
-                    break;
-                case UpgradeEnemy.UpgradeOne:
-
-                    transform.position = Vector2.Lerp(transform.position, playerTransform.position, speed);    
-                    break;
-                default:
-                    Debug.Log("Upgrade Types not valid");
-                    break;
-            }
+            float speed = 10f * Time.deltaTime;
+            var myPos = transform.position;
+            var otherPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+            var dist = (myPos - otherPos).magnitude;
+            var mapped = Mathf.InverseLerp(10, 5, dist);
+            transform.position = Vector2.MoveTowards(myPos, otherPos, -mapped * speed);
         }
-
-        if (currentEnemyType == EnemyTypes.RangedAI && currentUpgradeEnemy == UpgradeEnemy.UpgradeOne)
-        {
-            UseAbility();
-        }
-    }
-
-    IEnumerator ChargeAttack()
-    {
-        if (canCharge)
-        {
-            Debug.Log("Charging.... ");
-            yield return new WaitForSeconds(1f);
-            Debug.Log("Finish Charging");
-        }
-    }
-
-    
-
-    void UseAbility()
-    {
-        // if not available to use (still cooling down) just exit
-        if (IsAvailable == false)
-        {
-            return;
-        }
-
-        // made it here then ability is available to use...
-        // UseAbilityCode goes here
-
-        // start the cooldown timer
-        StartCoroutine(StartCooldown());
-    }
-
-    public IEnumerator StartCooldown()
-    {
-        IsAvailable = false;
-        Debug.Log("Start Cooldown");
-        yield return new WaitForSeconds(CooldownDuration);
-        IsAvailable = true;
     }
 }
