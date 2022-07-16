2060317a930d2f9e2b108fe32d8294b61b9b2733
Steventus <runescape_dudez@hotmail.com>
2022-07-16 15:53:25 +0800

    Player bullets destroyed on contact

--- Assets/Kiran - EnemyAI Script/EnemyAIManager.cs
+++ Assets/Kiran - EnemyAI Script/EnemyAIManager.cs
@@ -6,30 +6,23 @@ public class EnemyAIManager : MonoBehaviour
 {
     public float enemyHealth;
 
-    void Start()
-    {
-        enemyHealth = 100f;
-    }
-
-  
-    void Update()
-    {
-    
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "bullet")
         {
-            enemyHealth -= 10f;
-            Debug.Log(enemyHealth);
-            if (enemyHealth <= 0)
-            {
+            collision.gameObject.SetActive(false);
+            TakeDamage(10);
 
-                gameObject.SetActive(false);
-            }
         }
     }
 
+    private void TakeDamage(float _delta)
+    {
+        enemyHealth -= _delta;
+        if (enemyHealth <= 0)
+        {
 
+            gameObject.SetActive(false);
+        }
+    }
 }
